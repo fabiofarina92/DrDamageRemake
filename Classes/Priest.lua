f5f49745665cbed1f3cc55b447e91332e5c276df
Fabio <fabio.farina92@gmail.com>
2019-10-13 22:50:41 +1100

    yeah

--- Classes/Priest.lua
+++ Classes/Priest.lua
@@ -1,8 +1,44 @@
 -- Short circuit to prevent loading non-needed data
 if select(2, UnitClass("player")) ~= "PRIEST" then return end
+local critModifier = 1.5
+local Utils = DrDamageRemake
 
 function DrDamageRemake:PlayerData()
     self.classColour = 'e3e3e3'
     self.spellInfo = {
+        [585] = {
+            ["name"] = "Smite",
+            ["id"] = 585,
+            ["rank"] = 1,
+            ["data"] = {
+                lowerBound = 13,
+                upperBound = 17,
+                cost = 20,
+                castTime = 1.5,
+                mod = 0.10,
+                cooldown = {standard = 0, gcd = 1.5}
+            },
+            ["info"] = {school = {"Holy"}},
+            ["toolTipData"] = {
+                [1] = {
+                    label = "Average",
+                    type = 'Damage',
+                    calculation = (function(data)
+                        local amount = Utils:DefaultDamageCalculationAmount(data)
+                        return string.format("%s (%s - %s)", amount, data.lowerBound,
+                         data.upperBound)
+                    end)
+                },
+                [2] = {
+                    label = "Average",
+                    type = 'Damage',
+                    calculation = (function(data)
+                        local amount = Utils:DefaultCritDamageCalculationAmount(data, critModifier)
+                        return string.format("%s (%s - %s)", amount, data.lowerBound * critModifier,
+                         data.upperBound * critModifier)
+                    end)
+                },
+            }
+        }
     }
 end
--- Classes/Warrior.lua
+++ Classes/Warrior.lua
@@ -1,63 +1,9 @@
 if select(2, UnitClass("player")) ~= "WARRIOR" then return end
-local GetManaRegen = GetManaRegen
-local UnitPower = UnitPower
-local UnitPowerMax = UnitPowerMax
-local UnitAttackPower = UnitAttackPower
 local UnitStat = UnitStat
 local GetInventoryItemLink = GetInventoryItemLink
 local GetShapeshiftForm = GetShapeshiftForm
 local critModifier = 2.0
-
-function DrDamageRemake:getCurrentAttackPower(baseAp)
-    local base, posBuff, negBuff = UnitAttackPower("player")
-    if baseAp then
-        return base
-    end
-
-    local totalAttackPower = base + posBuff + negBuff
-
-    return totalAttackPower
-end
-
-function DrDamageRemake:defaultDamageCalculationAmount(data)
-    local base, posBuff, negBuff = UnitAttackPower("player")
-
-    local totalAttackPower = base + posBuff + negBuff
-    local average = math.floor((data.lowerBound + data.upperBound) / 2)
-    local modifiers = totalAttackPower * data.attackPowerMod
-    local calculation = average + modifiers
-    return calculation
-end
-
-function DrDamageRemake:defaultCritDamageCalculationAmount(data)
-    return self:defaultDamageCalculationAmount(data) * critModifier
-end
-
-function DrDamageRemake:damagePerRage(data)
-    return self:defaultDamageCalculationAmount(data) / data.rageCost
-end
-
-function DrDamageRemake:damagePerSecond(data)
-    local castsPerMinute = self:castsPerMinute(data)
-
-    local totalDamagePerMinute = castsPerMinute *
-                                     self:defaultDamageCalculationAmount(data)
-    return round(totalDamagePerMinute / 60, 2)
-end
-function DrDamageRemake:castsPerMinute(data)
-    local castsPerMinute = 0
-    if data.cooldown.gcd > data.cooldown.standard then
-        castsPerMinute = 60 / data.cooldown.gcd
-    else
-        castsPerMinute = 60 / data.cooldown.standard
-    end
-    return castsPerMinute
-end
-
-function DrDamageRemake:damagePerSecondCalc(castsPerMinute, damage)
-    local totalDamagePerMinute = castsPerMinute * damage
-    return round(totalDamagePerMinute / 60, 2)
-end
+local Utils = DrDamageRemake
 
 function DrDamageRemake:getBlockValue(data)
     local stat, effectiveStat, posBuff, negBuff = UnitStat("player", 1)
@@ -80,7 +26,7 @@ function DrDamageRemake:PlayerData()
                 upperBound = 5,
                 rageCost = 10,
                 castTime = 0,
-                attackPowerMod = 0,
+                mod = 0,
                 cooldown = {standard = 0, gcd = 1.5}
             },
             ["info"] = {school = {"Physical"}},
@@ -89,28 +35,28 @@ function DrDamageRemake:PlayerData()
                     label = "Average",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:defaultDamageCalculationAmount(data)
+                        return self:DefaultDamageCalculationAmount(data)
                     end)
                 },
                 [2] = {
                     label = "Crit",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:defaultCritDamageCalculationAmount(data)
+                        return self:DefaultCritDamageCalculationAmount(data, critModifier)
                     end)
                 },
                 [3] = {
                     label = "DPR",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:damagePerRage(data)
+                        return self:DamagePerPowerCost(data, data.rageCost)
                     end)
                 },
                 [4] = {
                     label = "DPS",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:damagePerSecond(data)
+                        return self:DamagePerSecond(data)
                     end)
                 }
             }
@@ -124,7 +70,7 @@ function DrDamageRemake:PlayerData()
                 upperBound = 276,
                 rageCost = 20,
                 castTime = 0,
-                attackPowerMod = 0,
+                mod = 0,
                 cooldown = {standard = 6, gcd = 1.5}
             },
             ["info"] = {school = {"Physical"}},
@@ -135,7 +81,7 @@ function DrDamageRemake:PlayerData()
                     calculation = (function(data)
                         self:getBlockValue(data)
                         return string.format("%s (%s - %s)",
-                                             self:defaultDamageCalculationAmount(
+                                             self:DefaultDamageCalculationAmount(
                                                  data), (data.lowerBound),
                                              (data.upperBound))
                     end)
@@ -145,8 +91,8 @@ function DrDamageRemake:PlayerData()
                     type = 'Damage',
                     calculation = (function(data)
                         return string.format("%s (%s - %s)",
-                                             self:defaultCritDamageCalculationAmount(
-                                                 data),
+                                             self:DefaultCritDamageCalculationAmount(
+                                                 data, critModifier),
                                              (data.lowerBound * critModifier),
                                              (data.upperBound * critModifier))
                     end)
@@ -155,14 +101,14 @@ function DrDamageRemake:PlayerData()
                     label = "DPR",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:damagePerRage(data)
+                        return self:DamagePerPowerCost(data, data.rageCost)
                     end)
                 },
                 [4] = {
                     label = "DPS",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:damagePerSecond(data)
+                        return self:DamagePerSecond(data)
                     end)
                 }
             }
@@ -176,7 +122,7 @@ function DrDamageRemake:PlayerData()
                 upperBound = 0,
                 rageCost = 30,
                 castTime = 0,
-                attackPowerMod = 0.45,
+                mod = 0.45,
                 cooldown = {standard = 6, gcd = 1.5}
             },
             ["info"] = {school = {"Physical"}},
@@ -185,31 +131,31 @@ function DrDamageRemake:PlayerData()
                     label = "Average (at 0 armour)",
                     type = 'Damage',
                     calculation = (function(data)
-                        return (self:getCurrentAttackPower(false) *
-                                   data.attackPowerMod)
+                        return (Utils:GetCurrentAttackPower(false) *
+                                   data.mod)
                     end)
                 },
                 [2] = {
                     label = "Crit (at 0 armour)",
                     type = 'Damage',
                     calculation = (function(data)
-                        return (self:getCurrentAttackPower(false) *
-                                   data.attackPowerMod) * 2
+                        return (Utils:GetCurrentAttackPower(false) *
+                                   data.mod) * 2
                     end)
                 },
                 [3] = {
                     label = "DPR",
                     type = 'Damage',
                     calculation = (function(data)
-                        return (self:getCurrentAttackPower() *
-                                   data.attackPowerMod) / data.rageCost
+                        return (Utils:GetCurrentAttackPower() *
+                                   data.mod) / data.rageCost
                     end)
                 },
                 [4] = {
                     label = "DPS",
                     type = 'Damage',
                     calculation = (function(data)
-                        return self:damagePerSecond(data)
+                        return self:DamagePerSecond(data)
                     end)
                 }
             }
