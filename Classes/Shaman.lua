1841fb5733b3b107213542fd9b6d5a545a1c1759
Fabio <fabio.farina92@gmail.com>
2019-11-25 01:47:06 +1100

    Fix shaman class to match new format

--- Classes/Shaman.lua
+++ Classes/Shaman.lua
@@ -8,29 +8,15 @@ function DrDamageRemake:PlayerData()
     self.spellInfo = {
         ['Lightning Bolt'] = {
             ["name"] = "Lightning Bolt",
-            [403] = {
-                ["rank"] = 1,
-                ["data"] = {
-                    lowerBound = 12,
-                    upperBound = 15,
-                    cost = 15,
-                    castTime = 1.5,
-                    mod = 0.10,
-                    cooldown = { standard = 0, gcd = 1.5 },
-                    level = { min = 1, max = 5 }
-                },
+            ["match"] = {
+                ["damageRange"] = '(%d+) to (%d+)',
+                ["cooldown"] = '(%d+) sec cooldown',
+                ["cost"] = '(%d+) Mana'
             },
-            [529] = {
-                ["rank"] = 2,
-                ["data"] = {
-                    lowerBound = 26,
-                    upperBound = 30,
-                    cost = 30,
-                    castTime = 2.0,
-                    mod = 0.10,
-                    cooldown = { standard = 0, gcd = 1.5 },
-                    level = { min = 1, max = 5 }
-                },
+            ["data"] = {
+                mod = 0.10,
+                cooldown = { standard = 0, gcd = 1.5 },
+                level = { min = 1, max = 5 },
             },
             ["info"] = { school = { "Nature" } },
             ["toolTipData"] = {
@@ -38,21 +24,34 @@ function DrDamageRemake:PlayerData()
                     label = "Average",
                     type = 'Damage',
                     calculation = (function(data)
-                        local range = Utils:GetSpellDamageRange(data.level.min, data.level.max, data.lowerBound, data.upperBound)
-                        local amount = Utils:DefaultSpellDamageCalculationAmount(range.newLower, range.newUpper, data.mod)
-                        return string.format("%s (%s - %s)", amount, range.newLower,
-                            range.newUpper)
+                        local amount = Utils:DefaultSpellDamageCalculationAmount(data.lowerBound, data.upperBound, data.mod)
+                        return string.format("%s (%s - %s)", amount, data.lowerBound,
+                            data.upperBound)
                     end)
                 },
-                [2] = {
+            }
+        },
+        ['Earth Shock'] = {
+            ["name"] = "Earth Shock",
+            ["match"] = {
+                ["damageRange"] = '(%d+) to (%d+)',
+                ["cooldown"] = '(%d+) sec cooldown',
+                ["cost"] = '(%d+) Mana'
+            },
+            ["data"] = {
+                mod = 0.10,
+                cooldown = { standard = 0, gcd = 1.5 },
+                level = { min = 1, max = 5 },
+            },
+            ["info"] = { school = { "Nature" } },
+            ["toolTipData"] = {
+                [1] = {
                     label = "Average",
                     type = 'Damage',
                     calculation = (function(data)
-                        local range = Utils:GetSpellDamageRange(data.level.min, data.level.max, data.lowerBound, data.upperBound)
-                        local amount = Utils:DefaultSpellDamageCalculationAmount(range.newLower, range.newUpper, data.mod)
-                        local critAmount = Utils:DefaultSpellCritDamageCalculationAmount(amount, critModifier)
-                        return string.format("%s (%s - %s)", critAmount, data.lowerBound * critModifier,
-                            data.upperBound * critModifier)
+                        local amount = Utils:DefaultSpellDamageCalculationAmount(data.lowerBound, data.upperBound, data.mod)
+                        return string.format("%s (%s - %s)", amount, data.lowerBound,
+                            data.upperBound)
                     end)
                 },
             }
